78215d811e053e50c50083e84d6c8a195a0d0109
armanul_alam <armaanularmaan@gmail.com>
2025-06-15 19:25:25 +0600

    file structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project DentBot',
+    version = "0.0.0",
+    author = "Md Armanul Alam",
+    author_email = "armaanularmaan@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
